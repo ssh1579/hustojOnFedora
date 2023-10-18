3dc39d42cd70a6e14607bdde279ce4e25156b913
Shihao Shen <116155969+ssh1579@users.noreply.github.com>
2023-10-18 18:42:46 +0800

    Delete trunk/install directory

--- trunk/install/cleanproblem.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/judge/src/install
-rm -rf ../../data/*
-echo "delete from problem ;" |./mysql.sh
-echo "ALTER TABLE problem AUTO_INCREMENT = 1000 ;"|./mysql.sh
--- trunk/install/update-sources-ubuntu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Official
-sed -i 's@//.*archive.ubuntu.com@//mirrors.aliyun.com@g' /etc/apt/sources.list
-sed -i 's@//security.ubuntu.com@//mirrors.aliyun.com@g' /etc/apt/sources.list
-
-# Tencent Cloud
-sed -i 's@//mirrors.tencentyun.com@//mirrors.aliyun.com@g' /etc/apt/sources.list
-
-apt update
