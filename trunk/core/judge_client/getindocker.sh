253ba945edcaa34cca5c35adeaf1f72df7783b00
Shihao Shen <116155969+ssh1579@users.noreply.github.com>
2023-10-18 18:41:49 +0800

    Delete trunk/core directory

--- trunk/core/judge_client/getindocker.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/bin/docker container run --pids-limit 100 --rm --cap-add SYS_PTRACE --net=host -v /home/judge:/home/judge -it hustoj
--- trunk/core/judge_client/loggedcalls.sh
+++
@@ -1,7 +0,0 @@
-
-CALLS=""
-for x in `grep CALLID\: /home/judge/log/client.log|awk -F: '{print $4}'|awk '{print $1}'|sort -nu `
-do
-     CALLS="$CALLS,$x"
-done
-echo "int LANG_CV[CALL_ARRAY_SIZE]={0$CALLS,0};"
